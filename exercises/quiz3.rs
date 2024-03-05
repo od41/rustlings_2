d6ce808c1e6d44d4f40c25dea29e1705a3b18ce6
Odafe Aror <odafe41@gmail.com>
2024-03-05 02:11:36 +0000

    solved lifetimes

--- exercises/quiz3.rs
+++ exercises/quiz3.rs
@@ -16,7 +16,9 @@
 //
 // Execute `rustlings hint quiz3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+pub trait Grade {
+   fn print(&self) -> String;
+}
 
 pub struct ReportCard {
     pub grade: f32,
@@ -24,8 +26,21 @@ pub struct ReportCard {
     pub student_age: u8,
 }
 
-impl ReportCard {
-    pub fn print(&self) -> String {
+impl Grade for ReportCard {
+    fn print(&self) -> String {
+        format!("{} ({}) - achieved a grade of {}",
+            &self.student_name, &self.student_age, &self.grade)
+    }
+}
+
+pub struct ReportCardLetters {
+    pub grade: String,
+    pub student_name: String,
+    pub student_age: u8,
+}
+
+impl Grade for ReportCardLetters {
+    fn print(&self) -> String {
         format!("{} ({}) - achieved a grade of {}",
             &self.student_name, &self.student_age, &self.grade)
     }
@@ -51,6 +66,17 @@ mod tests {
     #[test]
     fn generate_alphabetic_report_card() {
         // TODO: Make sure to change the grade here after you finish the exercise.
+        let report_card = ReportCardLetters {
+            grade: "A+".to_string(),
+            student_name: "Gary Plotter".to_string(),
+            student_age: 11,
+        };
+        assert_eq!(
+            report_card.print(),
+            "Gary Plotter (11) - achieved a grade of A+"
+        );
+
+        // grade number test
         let report_card = ReportCard {
             grade: 2.1,
             student_name: "Gary Plotter".to_string(),
@@ -58,7 +84,7 @@ mod tests {
         };
         assert_eq!(
             report_card.print(),
-            "Gary Plotter (11) - achieved a grade of A+"
+            "Gary Plotter (11) - achieved a grade of 2.1"
         );
     }
 }
